21242d7ddb8ea8ea9394a8e40ec83bcfc416c905
timb15 <43254717+timb15@users.noreply.github.com>
2018-12-28 13:09:48 -0800

    add middleware function to check authentication

--- routes.js
+++ routes.js
@@ -8,24 +8,8 @@ const auth  = require('basic-auth');
 const bcrypt = require('bcryptjs');
 
 //returns currently authenticated user if no authentication sent it will send "Authentication Required" message
-router.get('/users', (req, res, next) => {
-    const credentials = auth(req);
-    
-    if(!credentials) return res.status(401).send();
-
-    User
-        .findOne({emailAddress: credentials.name})
-        .exec(function(err, user) {
-            if(err) return next (err);
-            if(!user) return res.status(401).send();
-
-            bcrypt.compare(credentials.pass, user.password, function(err, same) {
-                if(err) return next(err);
-                if(!same)  return res.status(401).send();
-
-                res.json(user);  
-            });
-        });
+router.get('/users', checkForAuthentication, function(req, res, next) {
+    res.json(req.user);
 });
 
 
@@ -41,7 +25,7 @@ router.post('/users', (req, res, next) => {
         }
 
         res.location('/');
-        res.status(204).send();
+        res.status(201).send();
     });
 });
 
@@ -69,40 +53,23 @@ router.get('/courses/:id', (req, res, next) => {
 });
 
 //Creates a course if the user exists in the database and attaches users id to the course, sets the Location header to the URI for the course, and returns no content
-router.post('/courses', (req, res, next) => {
-    const credentials = auth(req);
-    
-    if(!credentials) return res.status(401).send();
-    
-    User
-    .findOne({emailAddress: credentials.name})
-    .exec(function(err, user) {
-        if(err) return next (err);
-        if(!user) return res.status(401).send();
+router.post('/courses', checkForAuthentication, function(req,res,next) {     
+    req.body.user = req.user._id;
+    const newCourse = req.body;
 
-        bcrypt.compare(credentials.pass, user.password, function(err, same) {
-            if(err) return next(err);
-            if(!same)  return res.status(401).send();
-        
-         
-            req.body.user = user._id;
-            const newCourse = req.body;
-
-            Course.create(newCourse, function(err, course) {
-                if(err){
-                    if(err.name === 'ValidationError') return res.status(400).send(err.message);
-                    return next(err);
-                }
-                res.location(`localhost:5000/api/courses/${course._id}`);
-                res.status(204).send();
-            }); 
-        });
-    });    
+    Course.create(newCourse, function(err, course) {
+        if(err){
+            if(err.name === 'ValidationError') return res.status(400).send(err.message);
+            return next(err);
+        }
+        res.location(`localhost:5000/api/courses/${course._id}`);
+        res.status(201).send();
+    }); 
 });
 
 
-//Updates a course if the user has authorizaton and returns no content
-router.put('/courses/:id', (req, res, next) => {
+//Updates a course if the user is the owner of the course and returns no content
+router.put('/courses/:id', checkForAuthentication, function(req, res, next) {
     Course
         .findById(req.params.id)
         .populate('user')
@@ -111,57 +78,58 @@ router.put('/courses/:id', (req, res, next) => {
                 if(err.name === 'ValidationError') return res.status(400).send(err.message);
                 return next(err);
             }
-            
-            const credentials = auth(req);
 
-            if(!credentials) {
+            if(course.user.emailAddress !== req.user.emailAddress || course.user.password !== req.user.password) {
                 return res.status(403).send();
             }
 
-            if(course.user.emailAddress !== credentials.name) {
-                return res.status(403).send();
-            }
-            bcrypt.compare(credentials.pass, course.user.password, function(err, same) {
+            course.set(req.body);
+            course.save(function (err, updatedCourse) {
                 if(err) return next(err);
-                if(!same)  return res.status(403).send();
-
-                course.set(req.body);
-                course.save(function (err, updatedCourse) {
-                    if(err) return next(err);
-                    res.status(204).send();
-                });
+                res.status(204).send();
             });
         });
 });
 
-//Deletes a course if the user has authorization and returns no content
-router.delete('/courses/:id', (req, res, next) => {
+//Deletes a course if the user is the owner of the course and returns no content
+router.delete('/courses/:id', checkForAuthentication, function(req, res, next) {
     Course
         .findById(req.params.id)
         .populate('user')
         .exec(function(err, course) {
             if(err) return next(err);
             
-            const credentials = auth(req);
-    
-            if(!credentials) {
+            if(course.user.emailAddress !== req.user.emailAddress || course.user.password !== req.user.password) {
                 return res.status(403).send();
             }
-    
-            if(course.user.emailAddress !== credentials.name) {
-                return  res.status(403).send();
-            }
 
-            bcrypt.compare(credentials.pass, course.user.password, function(err, same) {
-                if(err) return next(err);
-                if(same) {
-                    course.remove()
-                    res.status(204).send();
-                } else {
-                    res.status(403).send() 
-                }    
+            course.remove()
+            res.status(204).send();
             });
-    });
 });
+
+//checks if the users credentials match a user in the database and sets req.user to the user
+function checkForAuthentication(req, res, next) {
+    const credentials = auth(req);
+    
+    if(!credentials) {
+        return res.status(401).send();
+    }
+
+    User
+        .findOne({emailAddress: credentials.name})
+        .exec(function(err, user) {
+            if(err) return next (err);
+            if(!user) return res.status(401).send();
+
+            bcrypt.compare(credentials.pass, user.password, function(err, same) {
+                if(err) return next(err);
+                if(!same)  return res.status(401).send();
+
+                req.user = user;
+                next();  
+            });
+        });
+}
 
 module.exports = router;
